06eb05ab4ef90141d254b67555028050b66bdfa2
arkaanni <arkaannuruliman@gmail.com>
2023-06-04 12:56:18 +0700

    add dummy test [subject-service]

--- subject-service/src/test/kotlin/dev/arkaan/schoolapp/subjectservice/ApplicationTest.kt
+++
@@ -1,5 +0,0 @@
-package dev.arkaan.schoolapp.subjectservice
-
-
-class ApplicationTest {
-}
---
+++ subject-service/src/test/kotlin/dev/arkaan/schoolapp/subjectservice/DummyTest.kt
@@ -0,0 +1,11 @@
+package dev.arkaan.schoolapp.subjectservice
+
+import org.junit.Test
+
+class DummyTest {
+
+    @Test
+    fun dummy() {
+        assert(10 % 2 == 0)
+    }
+}
