7d49062addcd528415ef08b56fa8c84c10be5cc0
arkaanni <arkaannuruliman@gmail.com>
2023-06-09 17:51:12 +0700

    rename database [student-service]

--- student-service/src/test/resources/init_db.sql
+++ student-service/src/test/resources/init_db.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS `class`;
-CREATE TABLE `class` (
-  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
-  `class_id` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `student_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -16,32 +7,6 @@ CREATE TABLE `student` (
   `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `student_UN` (`student_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-DROP TABLE IF EXISTS `subject`;
-CREATE TABLE `subject` (
-  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
-  `subject_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `subject_UN` (`subject_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS `course_plan`;
-CREATE TABLE `course_plan` (
-  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `student_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `subject_code` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `semester` tinyint unsigned NOT NULL,
-  `year` year NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `course_plan_FK` (`subject_code`),
-  KEY `course_plan_FK_1` (`student_id`),
-  CONSTRAINT `course_plan_FK` FOREIGN KEY (`subject_code`) REFERENCES `subject` (`subject_code`),
-  CONSTRAINT `course_plan_FK_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `subject` VALUES (1,'MK01','Mathematics','Mathematics for first year.'),(2,'MK02','Biology','Biology');
 INSERT INTO `student` VALUES (1, '121983', 'John', 'Titor', 'Akihabara', '');
