7aea7f9862eede2c104d128d71597e9b59dda7c6
arkaanni <arkaannuruliman@gmail.com>
2023-06-15 21:26:17 +0700

    add cors [subject-service]

--- subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/SubjectServiceApp.kt
+++ subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/SubjectServiceApp.kt
@@ -1,8 +1,10 @@
 package dev.arkaan.schoolapp.subjectservice
 
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit =
@@ -11,6 +13,13 @@ fun main(args: Array<String>): Unit =
 fun Application.setUp() {
     install(ContentNegotiation) {
         jackson()
+    }
+    install(CORS){
+        allowHost("0.0.0.0:5173")
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
     }
 
     routing {
