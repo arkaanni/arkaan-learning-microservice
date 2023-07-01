638da9c690ad7d998caf8f14e6edaa733fed44b0
arkaanni <arkaannuruliman@gmail.com>
2023-07-01 12:35:51 +0700

    handle duplicate [subject-service]

--- subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/PostRoute.kt
+++ subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/PostRoute.kt
@@ -2,26 +2,39 @@ package dev.arkaan.schoolapp.subjectservice
 
 import dev.arkaan.schoolapp.subjectservice.db.withJdbi
 import dev.arkaan.schoolapp.subjectservice.domain.Subject
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.sql.SQLException
+import java.sql.SQLIntegrityConstraintViolationException
 
 fun Route.postRoute() {
     insertSubject()
 }
 
 fun Route.insertSubject() {
-    post("/subject") {
-        val subject = call.receive<Subject>()
-        this@insertSubject.withJdbi { jdbi ->
-            jdbi.inTransaction<Unit, SQLException> {
-                it.createUpdate("INSERT INTO subject (subject_code, name, description) VALUES(:code, :name, :desc)")
-                    .bind("code", subject.subjectCode)
-                    .bind("name", subject.name)
-                    .bind("desc", subject.description)
-                    .execute()
+    withJdbi { jdbi ->
+        post("/subject") {
+            val subject = call.receive<Subject>()
+            var status = HttpStatusCode.OK
+            var msg = "Success."
+            jdbi.withHandle<Unit, SQLException> {
+                try {
+                    it.createUpdate("INSERT INTO subject (subject_code, name, description) VALUES(?, ?, ?)")
+                        .bind(0, subject.subjectCode)
+                        .bind(1, subject.name)
+                        .bind(2, subject.description)
+                        .execute()
+                } catch (e: Exception) {
+                    if (e.cause is SQLIntegrityConstraintViolationException) {
+                        status = HttpStatusCode.BadRequest
+                        msg = "Subject already exists."
+                    }
+                }
             }
+            this.call.respond(status, msg)
         }
     }
 }
