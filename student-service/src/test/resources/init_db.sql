5611722593e5ed6b32e63ec17234f354436d3299
arkaanni <arkaannuruliman@gmail.com>
2023-07-09 23:04:23 +0700

    add semester to schema [student-service]

--- student-service/src/test/resources/init_db.sql
+++ student-service/src/test/resources/init_db.sql
@@ -5,8 +5,9 @@ CREATE TABLE `student` (
   `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `semester` tinyint unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `student_UN` (`student_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `student` VALUES (1, '121983', 'John', 'Titor', 'Akihabara', '');
+INSERT INTO `student` VALUES (1, '121983', 'John', 'Titor', 'Akihabara', '', 1);
