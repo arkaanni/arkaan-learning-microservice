10be41856f6ed508c7d4a8f0047d9abeffe16916
arkaanni <arkaannuruliman@gmail.com>
2023-07-09 18:12:50 +0700

    add subject [backoffice]

--- backoffice-app/src/routes/subject/add/+page.server.js
+++ backoffice-app/src/routes/subject/add/+page.server.js
@@ -1,5 +1,24 @@
 export const actions = {
-    addSubject: async (event) => {
-        // TODO
+    default: async ({ request }) => {
+        const formData = Object.fromEntries(await request.formData())
+        try {
+            const response = await fetch('http://127.0.0.1:8008/subject', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'text/html'
+                },
+                body: JSON.stringify(formData)
+            })
+            return {
+                success: response.status == 200 ? true : false,
+                msg: await response.text(),
+            }
+        } catch (e) {
+            return {
+                success: false,
+                msg: 'Something went wrong',
+            }
+        }
     }
 }
--- backoffice-app/src/routes/subject/add/+page.svelte
+++ backoffice-app/src/routes/subject/add/+page.svelte
@@ -1,4 +1,14 @@
-<form method="post" action="?/addSubject">
+<script>
+    import { enhance } from "$app/forms";
+
+    export let form;
+</script>
+
+{#if form}
+    <p>{form?.msg}</p>
+{/if}
+
+<form method="post" use:enhance>
     <div class="flex gap-4">
         <div class="flex flex-col gap-8 w-2/3">
             <div class="form-control">
@@ -6,6 +16,7 @@
                     <span class="label-text">Name</span>
                 </label>
                 <input
+                    name="name"
                     id="name"
                     type="text"
                     placeholder="Type here"
@@ -17,6 +28,7 @@
                     <span class="label-text">Code</span>
                 </label>
                 <input
+                    name="subjectCode"
                     id="code"
                     type="text"
                     placeholder="Type here"
@@ -28,6 +40,7 @@
                     <span class="label-text">Description</span>
                 </label>
                 <textarea
+                    name="description"
                     id="description"
                     type="text"
                     placeholder="Type here"
@@ -36,11 +49,11 @@
             </div>
             <div class="text-right">
                 <a href="/subject" class="btn btn-warning">Cancel</a>
-                <button class="btn btn-primary">Save</button>
+                <button type="submit" class="btn btn-primary">Save</button>
             </div>
         </div>
-        <div class="p-8">
-            <p class="text-xl">Add new Subject</p>
+        <div class="p-8 w-1/3">
+            <p class="text-xl text-center">Add new Subject</p>
         </div>
     </div>
 </form>
