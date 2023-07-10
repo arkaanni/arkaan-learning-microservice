4dc0cbb4bb37168db89877fab203bf00d4b16d2b
arkaanni <arkaannuruliman@gmail.com>
2023-07-10 23:11:17 +0700

    student list [backoffice]

--- backoffice-app/src/routes/student/+page.js
+++ backoffice-app/src/routes/student/+page.js
@@ -1,12 +1,12 @@
 export async function load({ fetch }) {
     try {
-        const subjects = await fetch('http://127.0.0.1:8008/subject', {
+        const students = await fetch('http://127.0.0.1:8443/students', {
             headers: {
                 'Accept': 'application/json',
             }
         })
         return {
-            subjects: subjects.json()
+            students: students.json()
         }
     } catch (error) {
         throw Error("Oopps..")
