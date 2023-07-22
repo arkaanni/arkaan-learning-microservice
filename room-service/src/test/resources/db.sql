8d914dfcd201cb76818a3cd16aa537de781bb2fd
arkaanni <arkaannuruliman@gmail.com>
2023-07-22 14:16:59 +0700

    add recurring schedule table [room-service]

--- room-service/src/test/resources/db.sql
+++ room-service/src/test/resources/db.sql
@@ -22,3 +22,18 @@ CREATE TABLE `schedule` (
   KEY `schedule_FK` (`room_id`),
   CONSTRAINT `schedule_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `recurring_schedule` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `day` tinyint unsigned NOT NULL,
+  `hour_start` tinyint unsigned NOT NULL,
+  `hour_end` tinyint unsigned NOT NULL,
+  `room_id` tinyint unsigned NOT NULL,
+  `minute_start` tinyint unsigned NOT NULL,
+  `minute_end` tinyint unsigned NOT NULL,
+  `from` timestamp NOT NULL,
+  `until` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recurring_schedule_FK` (`room_id`),
+  CONSTRAINT `recurring_schedule_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
