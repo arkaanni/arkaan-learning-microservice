6fb3d4ac43d88d36cd717eda4abc0698a03affe9
arkaanni <arkaannuruliman@gmail.com>
2023-07-22 12:16:24 +0700

    add schedule table [room-service]

--- room-service/src/test/resources/db.sql
+++ room-service/src/test/resources/db.sql
@@ -11,3 +11,14 @@ CREATE TABLE `room` (
   PRIMARY KEY (`id`),
   CONSTRAINT `room_FK` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `schedule` (
+  `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `description` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `start` datetime NOT NULL,
+  `until` datetime NOT NULL,
+  `room_id` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `schedule_FK` (`room_id`),
+  CONSTRAINT `schedule_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
