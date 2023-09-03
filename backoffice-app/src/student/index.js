02b2a1b40dcd52933e2c04433e4fbd710f81cdb9
arkaanni <arkaannuruliman@gmail.com>
2023-09-03 18:08:27 +0700

    fetch student list

--- backoffice-app/src/student/index.js
+++ backoffice-app/src/student/index.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+
+async function getAllStudents() {
+  return await fetch('http://localhost:8443/students')
+    .then(res => res.json());
+}
+
 function StudentComponent() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getAllStudents().then(data => {
+      setStudents(data);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Student List</h1>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Semester</th>
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(st => (
+              <tr key={st.student_id}>
+                <td>{st.student_id}</td>
+                <td>{st.first_name + " " + st.last_name}</td>
+                <td>{st.semester}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
 export default StudentComponent;
