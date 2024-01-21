b51ee4af4ee786e17a19be9e16975fa06c6229b7
arkaanni <arkaannuruliman@gmail.com>
2024-01-21 11:09:00 +0700

    endpoint for get recurring schedule

--- room-service/src/main/java/dev/arkaan/schoolapp/roomservice/schedule/RecurringSchedule.java
+++ room-service/src/main/java/dev/arkaan/schoolapp/roomservice/schedule/RecurringSchedule.java
@@ -1,14 +1,21 @@
 package dev.arkaan.schoolapp.roomservice.schedule;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 public record RecurringSchedule(
         String id,
+        @JsonProperty("room_id")
         short roomId,
         short day,
+        @JsonProperty("hour_start")
         short hourStart,
+        @JsonProperty("hour_end")
         short hourEnd,
+        @JsonProperty("minute_start")
         short minuteStart,
+        @JsonProperty("minute_end")
         short minuteEnd,
         LocalDate from,
         LocalDate until
