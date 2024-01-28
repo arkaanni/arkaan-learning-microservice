1ea640cbb3dec2adedc9ff928d29162f8b26f11b
arkaanni <arkaannuruliman@gmail.com>
2024-01-28 18:21:04 +0700

    swagger-ui config

--- swagger-ui/install.sh
+++ swagger-ui/install.sh
@@ -5,3 +5,6 @@ tar -xvf swagger-ui.tar.gz --strip 1 -C swagger
 docker build . -t  $containerRegistry/swagger-ui:latest
 docker push $containerRegistry/swagger-ui:latest
 docker rmi $containerRegistry/swagger-ui:latest
+
+rm -r swagger
+rm swagger-ui.tar.gz
