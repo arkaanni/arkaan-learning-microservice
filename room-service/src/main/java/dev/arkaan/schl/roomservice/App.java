fb0cd36665aa7473a819156fcedec0dbe91b577c
arkaanni <arkaannuruliman@gmail.com>
2024-01-28 12:18:37 +0700

    add springdoc

--- room-service/src/main/java/dev/arkaan/schl/roomservice/App.java
+++ room-service/src/main/java/dev/arkaan/schl/roomservice/App.java
@@ -1,9 +1,14 @@
 package dev.arkaan.schl.roomservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(title = "room-service")
+)
 public class App {
 
     public static void main(String[] args) {
