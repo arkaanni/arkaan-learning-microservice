2b34bae9af81784d7138d32f7af92c1bcb195d70
arkaanni <arkaannuruliman@gmail.com>
2024-02-05 22:17:42 +0700

    add db scripts

---
+++ db/init_db.sql
@@ -0,0 +1,93 @@
+CREATE DATABASE IF NOT EXISTS student;
+USE student;
+CREATE TABLE `student` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `semester` tinyint unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `student_UN` (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+
+CREATE DATABASE IF NOT EXISTS subject;
+USE subject;
+CREATE TABLE `subject` (
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
+  `subject_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `subject_UN` (`subject_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+
+CREATE DATABASE IF NOT EXISTS room;
+USE room;
+CREATE TABLE `category` (
+  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) UNIQUE COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `room` (
+  `id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `room_FK` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `schedule` (
+  `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `description` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `start` timestamp NOT NULL,
+  `until` timestamp NOT NULL,
+  `room_id` tinyint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `schedule_FK` (`room_id`),
+  CONSTRAINT `schedule_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `recurring_schedule` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `day` tinyint unsigned NOT NULL,
+  `hour_start` tinyint unsigned NOT NULL,
+  `hour_end` tinyint unsigned NOT NULL,
+  `room_id` tinyint unsigned NOT NULL,
+  `minute_start` tinyint unsigned NOT NULL,
+  `minute_end` tinyint unsigned NOT NULL,
+  `from` timestamp NOT NULL,
+  `until` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `recurring_schedule_FK` (`room_id`),
+  CONSTRAINT `recurring_schedule_FK` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+
+CREATE DATABASE IF NOT EXISTS course_plan;
+USE course_plan;
+CREATE TABLE `course_plan` (
+  `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `subject_code` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `semester` tinyint NOT NULL,
+  `year` year NOT NULL,
+  `schedule_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `enrollment` (
+  `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
+  `course_plan_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `student_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `grade` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `enrollment_FK` (`course_plan_id`),
+  CONSTRAINT `enrollment_FK` FOREIGN KEY (`course_plan_id`) REFERENCES `course_plan` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ db/init_user.sql
@@ -0,0 +1,11 @@
+CREATE USER IF NOT EXISTS 'student_service'@'%' IDENTIFIED BY 'student_service';
+CREATE USER IF NOT EXISTS 'subject_service'@'%' IDENTIFIED BY 'subject_service';
+CREATE USER IF NOT EXISTS 'room_service'@'%' IDENTIFIED BY 'room_service';
+CREATE USER IF NOT EXISTS 'course_plan_service'@'%' IDENTIFIED BY 'course_plan_service';
+
+GRANT ALL ON student.* TO 'student_service'@'%';
+GRANT ALL ON subject.* TO 'subject_service'@'%';
+GRANT ALL ON room.* TO 'subject_service'@'%';
+GRANT ALL ON course_plan.* TO 'course_plan_service'@'%';
+
+FLUSH PRIVILEGES;
