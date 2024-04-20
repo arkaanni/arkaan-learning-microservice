89c2aa9a7eac3e369f1bf15dbbbfd41a8e38cebc
arkaanni <arkaannuruliman@gmail.com>
2024-04-20 20:13:36 +0700

    main clazz

--- subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/SubjectServiceApp.kt
+++ subject-service/src/main/kotlin/dev/arkaan/schoolapp/subjectservice/SubjectServiceApp.kt
@@ -1,12 +1,15 @@
 package dev.arkaan.schoolapp.subjectservice
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
 import io.micronaut.runtime.Micronaut
-import jakarta.inject.Singleton
-import javax.sql.DataSource
 
-fun main(args: Array<String>) {
-    Micronaut.run(*args)
+class SubjectServiceApp {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Micronaut.run(*args)
+        }
+    }
 }
+
 
