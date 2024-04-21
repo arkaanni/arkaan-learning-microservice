88bac77c4eca95d9b6ecacc1a3d55293cb7c9b78
arkaanni <arkaannuruliman@gmail.com>
2024-04-21 11:51:51 +0700

    update test

--- subject-service/src/test/kotlin/dev/arkaan/schoolapp/subjectservice/E2ETest.kt
+++ subject-service/src/test/kotlin/dev/arkaan/schoolapp/subjectservice/E2ETest.kt
@@ -1,57 +1,58 @@
 package dev.arkaan.schoolapp.subjectservice
 
-import dev.arkaan.schoolapp.subjectservice.domain.Subject
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.serialization.jackson.*
-import io.ktor.server.config.*
-import io.ktor.server.testing.*
-import io.ktor.test.dispatcher.*
-import org.junit.AfterClass
-import org.junit.Assert.assertEquals
-import org.junit.BeforeClass
-import org.junit.ClassRule
-import org.junit.Test
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.PropertySource
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
-/**
- * Reference: https://gist.github.com/Stexxe/87b6a4985ceda80e1444208cb64826c4
- */
+@MicronautTest(application = SubjectServiceApp::class)
+@Testcontainers
 class E2ETest {
 
-    companion object {
-        private lateinit var server: TestApplicationEngine
+    @Inject
+    @Client("/")
+    lateinit var client: HttpClient
 
-        @ClassRule
+    @Inject
+    lateinit var app: EmbeddedServer
+
+    companion object {
+        private lateinit var server: EmbeddedServer
+
+        @Container
         @JvmField
         val mysql = MySQLContainer(DockerImageName.parse("mysql:8.0.32")).apply {
             withInitScript("init_db.sql")
+            waitingFor(Wait.forHealthcheck())
         }
 
-        @BeforeClass
+        @BeforeAll
         @JvmStatic
         fun setUp() {
-            server = TestApplicationEngine(createTestEnvironment {
-                developmentMode = false
-                config = MapApplicationConfig(
-                    "db.host" to mysql.host,
-                    "db.port" to mysql.firstMappedPort.toString(),
-                    "db.user" to mysql.username,
-                    "db.password" to mysql.password,
-                    "db.database" to mysql.databaseName,
-                    "db.driver" to "com.mysql.cj.jdbc.Driver"
-                )
-                module {
-                    setUp()
-                }
-            })
-            server.start(false)
+            server = ApplicationContext.run(EmbeddedServer::class.java, PropertySource.of(mapOf(
+                "datasource.subject.jdbc-url" to "jdbc:mysql://${mysql.host}:${mysql.firstMappedPort}/${mysql.databaseName}",
+                "datasource.subject.username" to mysql.username,
+                "datasource.subject.password" to mysql.password,
+                "datasource.subject.driver-class-name" to mysql.driverClassName
+            )), "test")
+            server.start()
         }
 
-        @AfterClass
+        @AfterAll
         @JvmStatic
         fun tearDown() {
             server.stop()
@@ -60,46 +61,62 @@ class E2ETest {
     }
 
     @Test
-    fun `get all subjects`() = testSuspend {
-        val response = server.client.get("/subject")
-        assertEquals(HttpStatusCode.OK, response.status)
-        // TODO :( ssdsddd
+    fun `should running`() {
+        Assertions.assertTrue(app.isRunning)
+        Assertions.assertTrue(client.isRunning)
     }
 
     @Test
-
-    fun `get subject by code`() = testSuspend {
+    fun `get by code`() {
         val code = "MK01"
-        val response = server.client.get("/subject/$code")
-        assertEquals(HttpStatusCode.OK, response.status)
-        assertEquals(
+        val response = client.toBlocking().retrieve("/subject/$code", HttpResponse::class.java)
+        Assertions.assertEquals(HttpStatus.OK, response.status)
+        Assertions.assertEquals(
             "{\"subject_code\":\"MK01\",\"name\":\"Mathematics\",\"description\":\"Mathematics for first year.\"}",
-            response.bodyAsText()
+            response.body().toString()
         )
     }
 
-    @Test
-    fun `insert subject should success`() = testSuspend {
-        val response = server.client
-            .config {
-                install(ContentNegotiation) { jackson() }
-            }.post("/subject") {
-                header("Content-Type", "application/json")
-                setBody(Subject("MP01", "Physics", "Physics"))
-            }
-        assertEquals(HttpStatusCode.OK, response.status)
-    }
+//    @Test
+//    fun `get all subjects`() = testSuspend {
+//        val response = server.client.get("/subject")
+//        assertEquals(HttpStatusCode.OK, response.status)
+        // TODO :( ssdsddd
+//    }
 
-    @Test
-    fun `insert subject should fail if already exists`() = testSuspend {
-        val response = server.client
-            .config {
-                install(ContentNegotiation) { jackson() }
-            }.post("/subject") {
-                header("Content-Type", "application/json")
-                setBody(Subject("MK01", "Physics", "Physics"))
-            }
-        assertEquals(HttpStatusCode.BadRequest, response.status)
-        assertEquals("Subject already exists.", response.bodyAsText())
-    }
+//    @Test
+//    fun `get subject by code`() = testSuspend {
+//        val code = "MK01"
+//        val response = client.toBlocking().retrieve("/subject/$code", HttpResponse::class.java)
+//        assertEquals(HttpStatus.OK, response.status)
+//        assertEquals(
+//            "{\"subject_code\":\"MK01\",\"name\":\"Mathematics\",\"description\":\"Mathematics for first year.\"}",
+//            response.body().toString()
+//        )
+//    }
+
+//    @Test
+//    fun `insert subject should success`() = testSuspend {
+//        val response = server.client
+//            .config {
+//                install(ContentNegotiation) { jackson() }
+//            }.post("/subject") {
+//                header("Content-Type", "application/json")
+//                setBody(Subject("MP01", "Physics", "Physics"))
+//            }
+//        assertEquals(HttpStatusCode.OK, response.status)
+//    }
+//
+//    @Test
+//    fun `insert subject should fail if already exists`() = testSuspend {
+//        val response = server.client
+//            .config {
+//                install(ContentNegotiation) { jackson() }
+//            }.post("/subject") {
+//                header("Content-Type", "application/json")
+//                setBody(Subject("MK01", "Physics", "Physics"))
+//            }
+//        assertEquals(HttpStatusCode.BadRequest, response.status)
+//        assertEquals("Subject already exists.", response.bodyAsText())
+//    }
 }
